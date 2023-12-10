6439fba76a199286c9a717d38d8cc50f2ddcc037
westermann <cwestermannie@gmail.com>
2023-12-10 20:00:32 +0100

    minor

--- 2023/day3/part1.c
+++ 2023/day3/part1.c
@@ -8,13 +8,23 @@
 typedef struct {
   char lines[MAX_LINES][MAX_CHAR];
   size_t n_lines;
+  size_t center_idx;
 } Data;
+
+typedef struct {
+  size_t start;
+  size_t end;
+  int num;
+} Number;
 
 typedef struct {
   char* previous;
   char* center;
   char* next;
 } LineWindow;
+
+Data read_data(char* filename);
+int next_window(Data* data, LineWindow* lw);
 
 Data read_data(char* filename) {
   FILE* input = fopen(filename, "r");
@@ -29,9 +39,25 @@ Data read_data(char* filename) {
   return data;
 }
 
+int next_window(Data* data, LineWindow* lw) {
+  if (!data->center_idx) {
+    lw->previous = 0;
+  } else {
+    lw->previous = data->lines[data->center_idx - 1];
+  }
+  lw->center = data->lines[data->center_idx];
+  lw->next = data->lines[data->center_idx + 1];
+  return EXIT_SUCCESS;
+}
+
 int main(int argc, char** argv) {
   Data data = read_data("input.txt");
+  LineWindow lw = {0};
+  next_window(&data, &lw);
 
+  printf("%s\n", lw.previous);
+  printf("%s\n", lw.center);
+  printf("%s\n", lw.next);
 
   return EXIT_SUCCESS;
 }
