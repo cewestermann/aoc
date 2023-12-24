35c16ed90c1d36ae0db4e377a1b351b038ce29d9
westermann <cwestermannie@gmail.com>
2023-12-24 15:46:17 +0100

    minor

--- 2023/day3/part1.c
+++ 2023/day3/part1.c
@@ -20,12 +20,15 @@ typedef struct {
 } PartNumber;
 
 bool is_digit(char const c);
+void print_perimiter(PartNumber num, LineWindow* lw);
+void print__single(PartNumber num, i32 len, String s);
+
 
 int main(int argc, char** argv) {
   String input = read_entire_file("input.txt");
   Strings lines = string_split_on(input, '\n');
 
-  LineWindow lw;
+  LineWindow lw = {0};
 
   for (size_t i = 0; i < lines.len; i++) {
     lw.center = lines.data[i];
@@ -51,24 +54,42 @@ int main(int argc, char** argv) {
           digit[dcount++] = lw.center.data[i++];
         }
         digit[dcount] = '\0';
-        numbers[pcount].end = i - 1;
+        numbers[pcount].end = i;
         numbers[pcount].value = atoi(digit);
         pcount++;
       }
     }
 
     for (size_t i = 0; i < pcount; i++) {
-      char printbuf[6];
-      printf("%d\n", numbers[i].value);
+      print_perimiter(numbers[i], &lw);
     }
   }
-
-
-
-
   return EXIT_SUCCESS;
 }
 
 bool is_digit (char const c) {
   return c <= '9' && c >= '0';
+}
+
+void print_perimiter(PartNumber num, LineWindow* lw) {
+  i32 len = (num.end + 1) - (num.start - 1);
+
+  if (lw->prev.data) {
+    print__single(num, len, lw->prev);
+  }
+
+  print__single(num, len, lw->center);
+
+  if (lw->next.data) { 
+    print__single(num, len, lw->next);
+  }
+  printf("\n");
+}
+
+void print__single(PartNumber num, i32 len, String s) {
+  char pbuf[8] = {0};
+
+  strncpy(pbuf, &s.data[num.start - 1], len);
+  printf("%s\n", pbuf);
+  pbuf[len] = '\0';
 }
